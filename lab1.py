6499c7f5d9a14cd60af4a5d42c5b1ffd380e58c9
ThatOneStas <salachnykstanislav0301@gmail.com>
2026-09-18 22:04:55 +0300

    Upload done lab.

---
+++ lab1.py
@@ -0,0 +1,20 @@
+import cmath
+
+# task1_1
+fruit = "grape"
+print("Task 1_1: ", fruit)
+
+# task1_2
+score = 75
+print("\nTask 1_2: ",75>=60)
+
+# task1_3
+is_even = False
+is_odd = True
+print("\nTask 1_3: ", is_even, is_odd)
+
+# task2
+x = 4.123
+y = 2.395
+c = cmath.log(x**3)+cmath.tan(y)+(y+x)**3-((x*cmath.sin(y+1.6)/(177*x**2)))
+print("\nTask 2: ", c)
---
+++ requests_example.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://www.example.com"
+
+response = requests.get(url)
+
+print(response.text)
